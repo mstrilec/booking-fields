4d8865449ef5be5d938208adec10db44b65f5316
mstrilec <MaxStrilec2005@gmail.com>
2025-04-03 14:56:33 +0300

    Created entities for DB

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from '../entities/booking.entity';
+import { Field } from '../entities/field.entity';
+import { User } from '../entities/user.entity';
 
 @Module({
   imports: [
@@ -13,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get('DATABASE_USERNAME'),
         password: configService.get('DATABASE_PASSWORD'),
         database: configService.get('DATABASE_NAME'),
-        entities: [__dirname + '/../**/*.entity.{ts,js}'],
+        entities: [User, Booking, Field],
         synchronize: configService.get('NODE_ENV') !== 'production',
       }),
     }),
